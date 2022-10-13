3fae84e2d6c8276deb15e98b2499be044f71ec6a
Dio Fitriadi <102437769+diofitriadi@users.noreply.github.com>
2022-10-13 16:11:15 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+/* Javascript */
+/* looping dan cetak nilai */
+
+/* Cetak program dari 1 - 100 */
+for (let i = 0; i <= 100; i++) {
+   /*  Jika nilai bisa dibagi tiga, cetak tiga */
+	if (i % 3 == 0) {
+  	console.log("Tiga")
+  } 
+  /*  Jika nilai bisa dibagi lima, cetak lima */
+  else if (i % 5 == 0) {
+    console.log("Lima")
+  } 
+  /* Jika nilai bisa dibagi tiga dan lima, cetak TigaLima */
+ 	else if (i % 3 == 0 && i % 5 == 0 ) {
+    console.log("TigaLima") 
+  }
+    /* Jika nilai tidak bisa dibagi tiga dan lima, maka cetak angka saja */
+  else console.log(i);
+}
